2ca5a16b1373c0f9fd80df0c74e43d627f548532
navinnep05 <navinkumartamilkumar@gmail.com>
2025-07-11 17:50:37 +0530

    first commit

---
+++ src/main/java/com/navin/Acting/Acting/ActingApplication.java
@@ -0,0 +1,13 @@
+package com.navin.Acting.Acting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/navin/Acting/Acting/ActingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.navin.Acting.Acting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
