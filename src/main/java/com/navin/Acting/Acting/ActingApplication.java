19508cab180e5cb7ac0835f38ba9924ab82b4f9c
navinnep05 <navinkumartamilkumar@gmail.com>
2025-12-05 07:35:45 +0530

    eureka server config completed

--- src/main/java/com/navin/Acting/Acting/ActingApplication.java
+++ src/main/java/com/navin/Acting/Acting/ActingApplication.java
@@ -2,8 +2,10 @@ package com.navin.Acting.Acting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ActingApplication {
 
 	public static void main(String[] args) {
